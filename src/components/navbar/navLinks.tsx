5fc4130790d4542ce70a41d3016beb4500fa6d9a
tommy-pellerin <tommytp@hotmail.fr>
2024-10-24 21:28:48 +0200

    dropdown styled

--- src/components/navbar/navLinks.tsx
+++ src/components/navbar/navLinks.tsx
@@ -2,38 +2,63 @@ import { useState } from "react";
 import { Link } from 'react-router-dom';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
-const links = [
-  { name: 'Trouver une séance', href: '/workouts', authRequired: false },
-  { name: 'Proposer une séance', href: '/host', authRequired: false },
-  { name: 'Inscription', href: '/sign-up', authRequired: false },
-  { name: 'Connexion', href: '/sign-in', authRequired: false },
-  { name: 'Messages', href: '/messages', authRequired: true },
-  { name: 'Mon compte', href: '/my-account', authRequired: true },
-  { name: 'Mes annonces', href: '/my-account', authRequired: true },
-  { name: 'Mes réservations', href: '/my-account', authRequired: true },
-  { name: 'Déconnexion', href: '/', authRequired: true },
-  { name: 'Aide', href: '/help', authRequired: false },
+const staticLinks = [
+  { name: 'Trouver une séance', href: '/workouts' },
+  { name: 'Proposer une séance', href: '/host' },
+];
 
+const notAuthLinks = [
+  { name: 'Inscription', href: '/sign-up' },
+  { name: 'Connexion', href: '/sign-in' },
+];
+const authLinks = [
+  { name: 'Messages', href: '/messages' },
+  { name: 'Mes réservations', href: '/my-account' },
+  { name: 'Mon compte', href: '/my-account' },
+  { name: 'Mes annonces', href: '/my-account'},
+  { name: 'Déconnexion', href: '/'},
 ];
 
 export default function NavLinks({ closeMenu }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false); // Simulate the auth state
-
-  // Filter links based on the authentication state
-  const filteredLinks = links.filter(link => {
-    // Show "authRequired" links only if authenticated, or non-authRequired otherwise
-    return isAuthenticated ? true : !link.authRequired;
-  });
+  const [isAuthenticated, setIsAuthenticated] = useState(true); // Simulate the auth state
 
   return (
     <ul>
-      {filteredLinks.map((link) => (
-        <li key={link.name} className="list-none hover:bg-gray-400">
-          <Link to={link.href} className="block w-full h-full p-3 md:pr-10"  onClick={closeMenu}>
-            {link.name}
-          </Link>
-        </li>
+      { staticLinks.map((link) => (
+          <li key={link.name} className="list-none hover:bg-gray-400 font-semibold">
+            <Link to={link.href} className="block w-full h-full p-3 md:pr-10"  onClick={closeMenu}>
+              {link.name}
+            </Link>
+          </li>
       ))}
+      
+      <hr />
+
+      { isAuthenticated ? 
+        authLinks.map((link) => (
+          <li key={link.name} className="list-none hover:bg-gray-400">
+            <Link to={link.href} className="block w-full h-full p-3 md:pr-10"  onClick={closeMenu}>
+              {link.name}
+            </Link>
+          </li>
+        ))
+        :
+        notAuthLinks.map((link) => (
+          <li key={link.name} className="list-none hover:bg-gray-400">
+            <Link to={link.href} className="block w-full h-full p-3 md:pr-10"  onClick={closeMenu}>
+              {link.name}
+            </Link>
+          </li>
+        ))
+      }
+
+      <hr />
+
+      <li className="list-none hover:bg-gray-400">
+        <Link to="/help" className="block w-full h-full p-3 md:pr-10"  onClick={closeMenu}>
+          Centre d'aide
+        </Link>
+      </li>
     </ul>
   );
 }
